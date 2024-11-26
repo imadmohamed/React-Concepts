0a23467338962603b6fd3b7dc919ee15947875fa
imad mohamed <abmohammedimad00@gmail.com>
2024-11-27 03:29:55 +0400

    useEffect completed

--- useEffect-Hook/src/App.jsx
+++ useEffect-Hook/src/App.jsx
@@ -2,19 +2,30 @@ import React, { useEffect, useState } from 'react'
 
 const App = () => {
 
-  const [num1, setNum1] = useState(100)
+  const [num1, setNum1] = useState(100);
+  const [num2, setNum2] = useState(1000);
 
   useEffect(()=>{
-    setNum1(2003)
-  },[])
+    // setNum1(200)
+    console.log("from useeffcet")
+    return ()=>{
+      setNum1(100);
+      console.log("cleanup1")
+      setNum2(1000)
+      console.log("cleanup2")
+    }
+  },[num1, num2])
 
-  console.log("num1", num1)
+//  console.log("num1", num1)
   
   
   return (
     <div>
       <h1>{num1}</h1>
-      <button onClick={()=>setNum1((currValue)=> currValue+1)}>Add</button>
+      <button onClick={()=>setNum1((curr)=> curr + 1)}>Add</button>
+
+      <h1>{num2}</h1>
+      <button onClick={()=>setNum2((curr)=> curr + 1)}>Add</button>
     </div>
   )
 }
