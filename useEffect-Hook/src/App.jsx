33ec85e49b15b940e234b38e68c8dd93f16b9f0e
imad mohamed <abmohammedimad00@gmail.com>
2024-11-27 02:55:48 +0400

    create a function

--- useEffect-Hook/src/App.jsx
+++ useEffect-Hook/src/App.jsx
@@ -7,12 +7,14 @@ const App = () => {
   useEffect(()=>{
     setNum1(2003)
   },[])
+
+  console.log("num1", num1)
   
   
   return (
     <div>
       <h1>{num1}</h1>
-      
+      <button onClick={()=>setNum1((currValue)=> currValue+1)}>Add</button>
     </div>
   )
 }
