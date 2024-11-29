63dc6b97b64de36e1381559b5ddbad90ba9fc9fc
imad mohamed <abmohammedimad00@gmail.com>
2024-11-29 18:21:28 +0400

    solve useState drop back

--- useRef-Hook/src/App.jsx
+++ useRef-Hook/src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const App = () => {
+
+  const [input, setInput] = useState("")
+  
+  const inputRef = useRef();
+  console.log("getting Render")
+
+  const display = () => {
+    console.log(inputRef.current.value)
+  }
+
   return (
-    <div>test</div>
+    <div>
+      <h1>Input</h1>
+      <input 
+      ref={inputRef}
+      type='text'
+      // value={input}
+      // onChange={(event) => setInput(event.target.value)}
+      />
+      <h1>My name is {inputRef.current?.value}</h1> 
+      <button onClick={display}>Show input</button>
+    </div>
   )
 }
 
 export default App
+
