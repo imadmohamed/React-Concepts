7d5f07c3a486eee6b53625c08c953dac042bb9f4
imad mohamed <abmohammedimad00@gmail.com>
2025-01-08 11:04:32 +0400

    useLayoutEffect hook is finish

--- useLayoutEffect-Hook/src/App.jsx
+++ useLayoutEffect-Hook/src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 
 const App = () => {
+
+  const [count, setCount] = useState(0)
+
+  useLayoutEffect(() =>{
+    console.log("count", count)
+  }, [count])
+ console.log("test")
   return (
-    <div>Aspp</div>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={()=> setCount((curr) => curr +1)}>Add</button>
+    </div>
   )
 }
 
