5681eb31bc9c9e0e65eeb15e7e1a5ea733cde011
imad mohamed <abmohammedimad00@gmail.com>
2025-01-08 10:27:14 +0400

    finish Reduser Hook

--- useReducer-Hook/src/App.jsx
+++ useReducer-Hook/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount((curr) => curr + 1);
+  };
+
+  const degrement = () => {
+    setCount((curr) => curr - 1);
+  };
+
   return (
-    <div>App</div>
+    <div style={{
+      display:"flex",
+      alignItems:"center"
+      
+    }}>
+      <button onClick={degrement}>-</button>
+      <h1>{count}</h1>
+      <button onClick={increment}>+</button>
+
+    </div>
   )
 }
 
