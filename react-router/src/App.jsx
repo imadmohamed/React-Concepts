e18e967f206046e350f6628b2a9219f9040e5032
imad mohamed <abmohammedimad00@gmail.com>
2025-01-09 14:46:13 +0400

    create nave bar

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from './Page/Home'
+import About from './Page/About'
+import Contact from './Page/Contact'
+import { User } from './Page/User'
+
 
 const App = () => {
   return (
-    <div>g</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element = {<Home />}/> 
+      <Route path='about' element = {<About />}/> 
+      <Route path='contact' element = {<Contact />}/> 
+      <Route path='user' element = {<User />}/> 
+    </Routes>
+    </BrowserRouter>
   )
 }
 
