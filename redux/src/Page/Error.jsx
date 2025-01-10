6a2aa61ff7f04cbc5b9bd0c7f887c8349c536c72
imad mohamed <abmohammedimad00@gmail.com>
2025-01-10 17:00:23 +0400

    requird codes copy paste from react router dom folder

---
+++ redux/src/Page/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>
+        <h1>404 Page Not found</h1>
+        <p>please check your route</p>
+    </div>
+  )
+}
+
+export default Error
---
+++ redux/src/components/NaveBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./NavBar.css";
+import { Link, useNavigate } from 'react-router-dom'
+
+const NaveBar = () => {
+  const navigate = useNavigate()
+
+  const handleChange = () => {
+    navigate("/user");
+  }
+
+  return (
+    <nav>
+        <h1>iDart</h1>
+        <ul>
+            <Link to = "/">Home</Link>
+            <Link to = "/about">About</Link>
+            {/* <Link to = "/user">User</Link> */}
+            <li onClick={handleChange}>user</li>
+            <Link to = "/contact">Contact</Link>
+            <Link to = "/username">User Name</Link>
+        </ul>
+    </nav>
+  );
+}
+
+export default NaveBar;
