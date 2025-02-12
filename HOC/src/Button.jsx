2bf331a39fc5bc2ef799b1cb4cf65ce7f851b0e9
imad mohamed <abmohammedimad00@gmail.com>
2025-02-13 02:42:27 +0400

    finish the HOC

---
+++ HOC/src/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const button = ({lable}) => {
+  return (
+    <button>
+        {lable}
+    </button>
+  )
+}
+
+export default button
---
+++ HOC/src/WithClickTracking.jsx
@@ -0,0 +1,16 @@
+const withClickTracking = (Comp) =>{
+    return (props) => {
+        const handleClick = ()=>{
+            console.log("Click Tracked", props.trackingInfo)
+        }
+
+        return (
+            <div onClick={handleClick}>
+                <Comp {...props}/>
+                
+            </div>
+        );
+    };
+};
+
+export default withClickTracking;
